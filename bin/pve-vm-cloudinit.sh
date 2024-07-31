09859bd9bae1b1743093b322e62d40b380301437
Jaime Jackson-Block <44634577+j1mbl3s@users.noreply.github.com>
2024-07-31 00:38:24 -0400

    chore: add license notice to the script

--- bin/pve-vm-cloudinit.sh
+++ bin/pve-vm-cloudinit.sh
@@ -1,5 +1,23 @@
 #!/bin/bash
 
+# pve-vm-cloudinit.sh - A set of Bash scripts to help manage the QEMU VM lifecycle
+# on Proxmox VE nodes for cloud-init VM templates with customized cloud-init configs.
+# https://github.com/j1mbl3s/pve-vm-cloudinit
+# Copyright (C) 2024  Jaime Jackson-Block
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 printUsage() {
     echo "\
 usage: $(basename $0)" \
